9be802ee3093df79c593dec51fa38b0b6e7159ad
LuaChristian <luachristixn@gmail.com>
2024-11-14 09:42:05 -0500

    :)

--- DatabaseCore/DatabaseComponents/Endpoints/Posts/InsertSampleData.cs
+++ DatabaseCore/DatabaseComponents/Endpoints/Posts/InsertSampleData.cs
@@ -10,7 +10,7 @@ public partial class RevMetrixDB
         using var connection = new SqlConnection(connectionString);
         await connection.OpenAsync();
         string insertQuery = @"
-            INSERT INTO [SensorData] (sensor_id, yaxis, xaxis, zaxis, brightness, samplenumber, timestamp) 
+            INSERT INTO [SensorData] (sensor_id, yaxis, xaxis, zaxis, brightness, samplenumber, [timestamp]) 
                 VALUES (@sensor_id, @yaxis, @xaxis, @zaxis, @brightness, @samplenumber, @TimeStamp)";
         
         using var command = new SqlCommand(insertQuery, connection);
