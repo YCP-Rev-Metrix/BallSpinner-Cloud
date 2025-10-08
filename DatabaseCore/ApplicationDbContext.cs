1dfc15f11c88aa75162390e0b8f5cee15f8b0c00
Emmet Larson <elars1002@gmail.com>
2025-10-08 18:11:45 -0400

    Automated DB migrations basic state, can create new class files but creating actual tables consistently is WIP

---
+++ DatabaseCore/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseCore.DatabaseComponents;
+using DatabaseCore.ServerTableFunctions.Fall2025DBTables;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<BallTable> Balls { get; set; }
+    public DbSet<UserTable> Users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost, 1433;User Id=SA; " +
+            "Password=Strong1PassMAN!!!;Database=sql_server;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<BallTable>().ToTable("Balls", schema:"combinedDB");
+        modelBuilder.Entity<UserTable>().ToTable("Users", schema:"combinedDB");
+    }
+}
